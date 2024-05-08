19792d45061cf74f444315effb2e4ba328f51bce
Abdul Samad Najm <82047394+sn1887@users.noreply.github.com>
2024-05-09 00:44:19 +0200

    Update feature engineering.py

--- utils/feature engineering.py
+++ utils/feature engineering.py
@@ -30,3 +30,63 @@ def dataPreprocessing(x):
 
 
 #------------------------------------------------------------------------------------------------------------
+# paragraph features
+def remove_punctuation(text):
+    """
+    Remove all punctuation from the input text.
+    
+    Args:
+    - text (str): The input text.
+    
+    Returns:
+    - str: The text with punctuation removed.
+    """
+    # string.punctuation
+    translator = str.maketrans('', '', string.punctuation)
+    return text.translate(translator)
+
+def Paragraph_Preprocess(tmp):
+    # Expand the paragraph list into several lines of data
+    tmp = tmp.explode('paragraph')
+    # Paragraph preprocessing
+    tmp = tmp.with_columns(pl.col('paragraph').map_elements(dataPreprocessing))
+    tmp = tmp.with_columns(pl.col('paragraph').map_elements(remove_punctuation).alias('paragraph_no_pinctuation'))
+    tmp = tmp.with_columns(pl.col('paragraph_no_pinctuation').map_elements(count_spelling_errors).alias("paragraph_error_num"))
+    # Calculate the length of each paragraph
+    tmp = tmp.with_columns(pl.col('paragraph').map_elements(lambda x: len(x)).alias("paragraph_len"))
+    # Calculate the number of sentences and words in each paragraph
+    tmp = tmp.with_columns(pl.col('paragraph').map_elements(lambda x: len(x.split('.'))).alias("paragraph_sentence_cnt"),
+                    pl.col('paragraph').map_elements(lambda x: len(x.split(' '))).alias("paragraph_word_cnt"),)
+    return tmp
+# feature_eng
+paragraph_fea = ['paragraph_len','paragraph_sentence_cnt','paragraph_word_cnt']
+paragraph_fea2 = ['paragraph_error_num'] + paragraph_fea
+def Paragraph_Eng(train_tmp):
+    num_list = [0, 50,75,100,125,150,175,200,250,300,350,400,500,600]
+    num_list2 = [0, 50,75,100,125,150,175,200,250,300,350,400,500,600,700]
+    aggs = [
+        # Count the number of paragraph lengths greater than and less than the i-value
+        *[pl.col('paragraph').filter(pl.col('paragraph_len') >= i).count().alias(f"paragraph_{i}_cnt") for i in [0, 50,75,100,125,150,175,200,250,300,350,400,500,600,700] ], 
+        *[pl.col('paragraph').filter(pl.col('paragraph_len') <= i).count().alias(f"paragraph_{i}_cnt") for i in [25,49]], 
+        # other
+        *[pl.col(fea).max().alias(f"{fea}_max") for fea in paragraph_fea2],
+        *[pl.col(fea).mean().alias(f"{fea}_mean") for fea in paragraph_fea2],
+        *[pl.col(fea).min().alias(f"{fea}_min") for fea in paragraph_fea2],
+        *[pl.col(fea).sum().alias(f"{fea}_sum") for fea in paragraph_fea2],
+        *[pl.col(fea).first().alias(f"{fea}_first") for fea in paragraph_fea2],
+        *[pl.col(fea).last().alias(f"{fea}_last") for fea in paragraph_fea2],
+        *[pl.col(fea).kurtosis().alias(f"{fea}_kurtosis") for fea in paragraph_fea2],
+        *[pl.col(fea).quantile(0.25).alias(f"{fea}_q1") for fea in paragraph_fea2],  
+        *[pl.col(fea).quantile(0.75).alias(f"{fea}_q3") for fea in paragraph_fea2],  
+        ]
+    
+    df = train_tmp.group_by(['essay_id'], maintain_order=True).agg(aggs).sort("essay_id")
+    df = df.to_pandas()
+    return df
+tmp = Paragraph_Preprocess(train)
+train_feats = Paragraph_Eng(tmp)
+train_feats['score'] = train['score']
+# Obtain feature names
+feature_names = list(filter(lambda x: x not in ['essay_id','score'], train_feats.columns))
+print('Features Number: ',len(feature_names))
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
